cbab3f80bc6d3f701cd8cb26e972d715118ccec4
薛贤俊 <hzxuexianjun@corp.netease.com>
2016-09-28 20:58:56 +0800

    处理图像弯曲

--- time/src/main/java/com/zxmn/time/view/WheelView.java
+++ time/src/main/java/com/zxmn/time/view/WheelView.java
@@ -1,24 +1,30 @@
 package com.zxmn.time.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
 import android.support.v4.content.ContextCompat;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Scroller;
 
 import com.zxmn.time.R;
 import com.zxmn.time.adapter.WheelAdapter;
 import com.zxmn.time.listeners.OnItemSelectedListener;
 import com.zxmn.time.listeners.OnWheelScrollListener;
-import com.zxmn.time.utils.DensityUtils;
 import com.zxmn.time.utils.Logger;
+
+import java.util.ArrayList;
 
 
 /**
@@ -34,7 +40,7 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
 
     private float mTextSize;
 
-    private float mScaleFactor = 1.2f;
+    private float mScaleFactor = 1.3f;
 
     private int mScrollY = 0;
 
@@ -45,6 +51,8 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
     private Paint mLinesPaint;
 
     private Paint mClearPaint;
+
+    private Paint mShadowPaint;
 
     private int mNormalColor;
 
@@ -57,7 +65,22 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
 
     private OnItemSelectedListener mSelectedListener;
 
+    private int mShadowHeight;
+
     private int mLastPosition = 0;
+
+    private LinearGradient mBlackToTransparent;
+    private LinearGradient mTransparentToBlack;
+
+    private Bitmap mCacheBitmap;
+
+    private Canvas mCacheCanvas;
+
+    private Path mPath;
+
+    private float[] mVerts;
+
+    private Camera mCamera;
 
     public WheelView(Context context) {
         this(context, null);
@@ -81,9 +104,25 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
 
         //init lines paint
         mLinesPaint = getPaint(getContext().getResources().getDimensionPixelSize(R.dimen.wheel_edge_width), mNormalColor);
+
+        //init shadow paint
+        initShadowPaint();
         //init clear paint
         initClearPaint();
         mTouchHelper = new WheelTouchHelper(this);
+        mShadowHeight = getResources().getDimensionPixelSize(R.dimen.wheel_shadow_height);
+
+        mCacheCanvas = new Canvas();
+
+        mCamera = new Camera();
+    }
+
+    private void initShadowPaint() {
+        mShadowPaint = new Paint();
+        mShadowPaint.setAntiAlias(true);
+        mShadowPaint.setStyle(Paint.Style.FILL);
+        mBlackToTransparent = new LinearGradient(0, 0, 0, 1, Color.parseColor("#30000000"), Color.TRANSPARENT, Shader.TileMode.CLAMP);
+        mTransparentToBlack = new LinearGradient(0, 0, 0, 1, Color.TRANSPARENT, Color.parseColor("#30000000"), Shader.TileMode.CLAMP);
     }
 
     private void initClearPaint() {
@@ -122,6 +161,7 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
                 mUpperEdge = getMeasuredHeight() / 2 - mItemHeight / 2;
                 mLowerEdge = mUpperEdge + mItemHeight;
                 mScrollY = mTotalHeight - getMeasuredHeight() / 2 + mItemHeight / 2;
+                initVerts();
                 invalidate();
             }
         });
@@ -147,22 +187,44 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
         super.onDraw(canvas);
         if (mAdapter == null)
             return;
-        //first we draw normal texts
-        drawContent(canvas);
-        //then clear center area
-        clearCenterArea(canvas);
-        //draw scaled text
-        drawFilteredText(canvas);
-        //draw two lines
-        drawEdges(canvas);
+        //drawContent(canvas);
+        // drawShadow(canvas);
+        //   drawFilteredText(canvas);
+        //   drawEdges(canvas);
+        drawCache();
+        //canvas.drawBitmap(mCacheBitmap, 0, 0, null);
+        canvas.drawBitmapMesh(mCacheBitmap, 1, mVerts.length / 4 - 1, mVerts, 0, null, 0, null);
+        // drawFilteredText(canvas);
     }
 
-    private void clearCenterArea(Canvas canvas) {
-        canvas.drawRect(0, mUpperEdge, getMeasuredWidth(), mLowerEdge, mClearPaint);
+    private void drawCache() {
+        //clear content
+        mCacheCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+        //first we draw normal texts
+
+
+        //first we draw normal texts
+        drawContent(mCacheCanvas);
+        //draw shadow
+//        drawShadow(mCacheCanvas);
+        //then clear center area
+        //  clearCenterArea(canvas);
+        //draw scaled text
+        //drawFilteredText(mCacheCanvas);
+        //draw two lines
+        // drawEdges(mCacheCanvas);
     }
 
     private void drawContent(Canvas canvas) {
+        canvas.save();
+        if (mPath == null) {
+            mPath = new Path();
+            mPath.addRect(0, 0, getMeasuredWidth(), mUpperEdge, Path.Direction.CW);
+            mPath.addRect(0, mLowerEdge, getMeasuredWidth(), getMeasuredHeight(), Path.Direction.CW);
+        }
+        //canvas.clipPath(mPath);
         drawTexts(mNormalPaint, canvas);
+        canvas.restore();
     }
 
     private void drawTexts(Paint paint, Canvas canvas) {
@@ -187,11 +249,18 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
         canvas.drawLine(0, mLowerEdge, getMeasuredWidth(), mLowerEdge, mLinesPaint);
     }
 
+    private void drawShadow(Canvas canvas) {
+        mShadowPaint.setShader(mBlackToTransparent);
+        canvas.drawRect(0, 0, getMeasuredWidth(), mShadowHeight, mShadowPaint);
+        mShadowPaint.setShader(mTransparentToBlack);
+        canvas.drawRect(0, getMeasuredHeight() - mShadowHeight, getMeasuredWidth(), getMeasuredHeight(), mShadowPaint);
+    }
+
     private void drawFilteredText(Canvas canvas) {
-        canvas.save(Canvas.CLIP_SAVE_FLAG);
+        canvas.save();
         canvas.clipRect(0, mUpperEdge, getMeasuredWidth(), mLowerEdge);
         drawTexts(mScaledPaint, canvas);
-        canvas.restoreToCount(Canvas.CLIP_SAVE_FLAG);
+        canvas.restore();
     }
 
     @Override
@@ -234,10 +303,8 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
     public void smoothScrollToPosition(int position) {
         int destY = amend(position * mItemHeight - mUpperEdge);
         int dy = mScrollY - destY;
-        Logger.d("destY " + destY);
-        Logger.d("position " + position);
         if (mTotalHeight - mScrollY < mItemHeight && destY == 0) {
-            dy = (mItemHeight - mTotalHeight + mScrollY) - mItemHeight;
+            dy = mScrollY - mTotalHeight;
         }
         if (position != mLastPosition && mSelectedListener != null)
             mSelectedListener.onItemSelected(position);
@@ -261,6 +328,82 @@ public class WheelView extends View implements OnWheelScrollListener, WheelAdapt
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
         setMeasuredDimension(widthSize, heightSize);
         mTouchHelper.setViewHeight(getMeasuredHeight());
+        if (mCacheBitmap == null) {
+            mCacheBitmap = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+            mCacheCanvas.setBitmap(mCacheBitmap);
+        }
+    }
+
+    private void initVerts() {
+        double r = getMeasuredHeight() / 2;
+        int k = 0;
+        mVerts = new float[4 * (getMeasuredHeight() + 1)];
+        float lastY = 0;
+        ArrayList<Float> topys = new ArrayList<>();
+        float biggestDelta = 0;
+        for (int i = 0; i <= getMeasuredHeight(); i++) {
+            float y = 0;
+
+            if (i <= r) {
+                y = (float) (r - Math.sqrt(r * r - i * i));
+                if (y - lastY >= biggestDelta)
+                    biggestDelta = y - lastY;
+                lastY = y;
+                topys.add(y);
+                if (y >= r - mItemHeight / 2) {
+                    break;
+                }
+            }
+        }
+
+
+        /*mVerts = new float[80];
+        double fromHeight = mItemHeight / 2;
+        double endHeight = 0.5 * mTextSize;
+        int n = 10;
+        double k = (getMeasuredHeight() / 2 - 10 * endHeight) / (n * (n + 1) / 2);
+
+        Logger.d("k " + k);
+        int j = 0;
+        float lastY = 0;
+
+        mVerts[j++] = 0;
+        mVerts[j++] = 0;
+        mVerts[j++] = getMeasuredWidth();
+        mVerts[j++] = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            lastY += (float) (endHeight + k * i);
+            mVerts[j++] = 0;
+            mVerts[j++] = lastY;
+            mVerts[j++] = getMeasuredWidth();
+            mVerts[j++] = lastY;
+            Logger.d("y " + (endHeight + k * i));
+        }
+
+        mVerts[j++] = 0;
+        mVerts[j++] = getMeasuredHeight() / 2;
+        mVerts[j++] = getMeasuredWidth();
+        mVerts[j++] = getMeasuredHeight() / 2;
+        lastY = getMeasuredHeight() / 2;
+        for (int i = 0; i < n - 1; i++) {
+            lastY += (float) (fromHeight - k * i);
+            mVerts[j++] = 0;
+            mVerts[j++] = lastY;
+            mVerts[j++] = getMeasuredWidth();
+            mVerts[j++] = lastY;
+            Logger.d("y " + (fromHeight - k * i));
+        }*/
+      /*  float factor = getMeasuredHeight()/5;
+        float r = getMeasuredWidth();
+        mVerts = new float[]{
+            0,0,r,0,
+            0,0.8f*factor,r,0.8f*factor,
+            0,mUpperEdge,r,mUpperEdge,
+            0,mLowerEdge,r,mLowerEdge,
+            0,4.2f*factor,r,4.2f*factor,
+            0,5f*factor,r,5f*factor
+        };*/
     }
 
     @Override
